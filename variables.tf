b631bdb6ac12b9577484178fa55823b0f8fafa90
AAbir <110724190+aabbirbapi@users.noreply.github.com>
2023-06-04 20:14:22 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,5 @@
+resource "azurerm_linux_virtual_machine_scale_set" "this" {
+  name                            = format("%s", local.vm_name)
+  resource_group_name             = var.resource_group_name
+  location                        = var.location
+  s
